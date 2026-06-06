ebf1f231efc6f61f2c40e3c8a75a8ebb32ce8cba
Devansh Pundir <pundirdevansh04@gmail.com>
2026-06-06 14:37:16 +0530

    connect to mongoatlas

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js"
+
+const connectDb = async()=>{
+    try{
+        console.log(process.env.MONGODB_URI);
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host} \n`);
+    }
+    catch(error){
+        console.log("Error: ",error);
+        process.exit(1);
+    }
+
+}
+
+export default connectDb;
