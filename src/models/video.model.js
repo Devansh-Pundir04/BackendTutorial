0ebc4c5e085d30bf33be62cca8522a107b58913a
Devansh Pundir <pundirdevansh04@gmail.com>
2026-06-06 15:58:08 +0530

    user models and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+
+
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type: String, //cloudinary url
+            required: true,
+        },
+        thumbnail:{
+            type: String, //cloudinary url
+            required: true,
+        },
+        title:{
+            type: String, 
+            required: true,
+        },
+        Description:{
+            type: String, 
+            required: true,
+        },
+        Duration:{
+            type: String, 
+            required: true,
+        },
+        views:{
+            type: Number,
+            default: 0,
+        },
+        isPublished:{
+            type: Boolean,
+            default: true,
+        },
+        owner:{
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+    },{timestamps:true});
+
+    
+export const Video = mongoose.model("Video",videoSchema);
